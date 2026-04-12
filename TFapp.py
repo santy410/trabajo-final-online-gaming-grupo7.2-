c3cc4324848664078b418ed5b2e92071d6854137
randyl21 <randylcondem@gmail.com>
2026-04-12 04:04:01 +0000

    se termina de revisar los datos

--- TFapp.py
+++ TFapp.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import streamlit as st 
+import pandas as pd
+import plotly.express as px
 
 #  Carga de datos
 nombre_archivo = "17. Online Gaming (5).csv"
@@ -11,9 +14,34 @@ variables_necesarias = ['Age', 'AvgSessionDurationMinutes', 'GameGenre', 'Engage
 print("conteo de datos nulos por cada variable ")
 print(df[variables_necesarias].isnull().sum())
 
-# 4. Una vista rápida para que veas tus 4 columnas
+# 4 vista rapida de las 4 variables 
 print("vista previa de datos")
 print(df[variables_necesarias].head())
 
+#tipos de datos
+
+print("tipos de datos")
+print(df[variables_necesarias].dtypes)
+
+print("cuales son los datos")
+
+# cuales generos de juego hay
+print(f"generos de juego encontrados: {df['GameGenre'].unique()}")
+
+# niveles de engagement
+print(f"niveles de compromiso: {df['EngagementLevel'].unique()}")
+
+print("rango de edad")
+print(f"La edad va desde los {df['Age'].min()} hasta los {df['Age'].max()} años.")
+
+print("conteo por cada variable")
+print("\nDistribución por Género de Juego:")
+print(df['GameGenre'].value_counts())
+
+print("\nDistribución por Nivel de Compromiso:")
+print(df['EngagementLevel'].value_counts())
+
+
+            
 
 
